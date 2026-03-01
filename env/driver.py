d5deaa7e8cca2a4e35f89ce896721b748fc0ee5a
tvvan99 <tranvan99py@gmail.com>
2026-03-01 08:19:05 +0700

    Update driver.py
    Update to new method for run

--- env/driver.py
+++ env/driver.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 def create_driver(headless: bool = True):
-    options = Options
+    options = Options()
     if headless:
         options.add_argument("--headless")
-    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=options)
